d5ea255d46d58928392a2f9e25d443f2a55e3748
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-07-20 21:13:11 +0300

    I added new project

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+
+
+btn.onClick=function(){
+  sidebar.classList.toggle(".active")
+}
+btn.onClick=function(){
+  sidebar.classList.toggle(".active")
+}
